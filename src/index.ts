df732d2d7ab077f66f623d0a9d7c76e2af22e5af
Martin Palkov <martin.palkov@gmail.com>
2024-03-18 21:25:38 +0100

    habilitar .env (npm i dotenv)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, {Express} from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app: Express = express();
-const port = /* process.env.PORT || */ 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port} ✅`);
